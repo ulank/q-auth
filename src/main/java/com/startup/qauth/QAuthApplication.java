269e6ddc064f900edfb0b1fa183b678c8604ce48
Ernur <ulankdt@gmail.com>
2022-06-20 16:54:17 +0600

    Initial commit

---
+++ src/main/java/com/startup/qauth/QAuthApplication.java
@@ -0,0 +1,13 @@
+package com.startup.qauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/startup/qauth/QAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.startup.qauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
